b50e1dbb93206ff45da4b682d5bf7c497c3cb3de
ViktoriiaGaisina <viktoriiagaisina@gmail.com>
2025-08-15 17:27:26 +0300

    openApi bd

--- src/main/java/com/otp/curensyservis/CurensyServisApplication.java
+++ src/main/java/com/otp/curensyservis/CurensyServisApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CurensyServisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurensyServisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CurensyServisApplication.class, args);
+    }
 
 }
