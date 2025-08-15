aceaebef64518afed3431eb0f6c666880d50dc2b
ViktoriiaGaisina <viktoriiagaisina@gmail.com>
2025-08-15 17:14:03 +0300

    Initial commit

---
+++ src/main/java/com/otp/curensyservis/CurensyServisApplication.java
@@ -0,0 +1,13 @@
+package com.otp.curensyservis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurensyServisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurensyServisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otp/curensyservis/CurensyServisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otp.curensyservis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurensyServisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
